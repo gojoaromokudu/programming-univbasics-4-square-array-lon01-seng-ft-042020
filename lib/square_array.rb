11f3d85f6bea98d680d2f6b169188bed5c35004a
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-02-21 23:45:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
 def square_array(array)
- squares = []
+
  i = 0
-  while i < array.length
-  
-  do
+  while i < array.length do
     squares.push(array[i]*array[i])
     i+=
   end
-  return squares
+  squares
 end
